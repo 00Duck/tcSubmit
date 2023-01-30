522081d8066ef898106f087b6979e1d40f5b9a76
Blake <blake.duckworth@outlook.com>
2023-01-30 02:00:16 -0500

    update config to use file dir

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tcSubmit
+
+Add to .zshrc (replace folder with yours):
+
+`alias submit="python3 /Users/blake/Documents/Programs/tcSubmit/main.py"`
--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import json
-import sys
+import os
 
 class Configuration:
     def __init__(self) -> None:
@@ -11,7 +11,10 @@ class Configuration:
         self.password = ""
     
     def load(self) -> Configuration:
-        with open("config.json") as conf:
+        #gives us directory of this script
+        file_dir = os.path.realpath(os.path.dirname(__file__))
+        config_dir = os.path.join(file_dir, "config.json")
+        with open(config_dir) as conf:
             configJSON = json.load(conf)
             self.base_path = configJSON["base_path"]
             self.instance_name = configJSON["instance_name"]
